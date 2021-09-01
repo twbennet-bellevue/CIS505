017b5adbfe18e68acf5f44f9f90e402967f13a18
Aetrius <tylerwbennet@gmail.com>
2021-08-31 22:27:15 -0500

    initial commit

---
+++ Module_1/HelloWorldApp/BennetHelloWorld.java
@@ -0,0 +1,7 @@
+public class BennetHelloWorld {
+    private static String FIRST_NAME = "Tyler";
+    private static String LAST_NAME = "Bennet";
+    public static void main(String[] args) {
+        System.out.printf("%s %s", FIRST_NAME, LAST_NAME);
+    }
+}
