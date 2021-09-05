a968dae8cbfa84a018ed123ca819a6cb9119a18e
Aetrius <tylerwbennet@gmail.com>
2021-09-04 22:49:18 -0500

    Updated template and adding variables

---
+++ Module_1/FanApp/Fan.java
@@ -0,0 +1,9 @@
+/*
+    Bennet, T. (2021). CIS 505 Intermediate Java Programming. Bellevue University
+*/
+
+public class Fan {
+    public Fan() {
+
+    }
+}
---
+++ Module_1/FanApp/TestFanApp.java
@@ -0,0 +1,12 @@
+package Module_1.FanApp;
+
+/*
+    Bennet, T. (2021). CIS 505 Intermediate Java Programming. Bellevue University
+*/
+public class TestFanApp {
+
+    public static void main(String[] args) {
+        Fan test = new Fan();
+
+    } //end main
+} //end BennetHelloWorld
