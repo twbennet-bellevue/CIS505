358eed082d115e31435ecc314687d46d0f99392e
Tyler B <tylerwbennet@gmail.com>
2021-09-08 18:00:05 -0500

    updated packages

--- Module_1/FanApp/Fan.java
+++ Module_1/FanApp/Fan.java
@@ -1,3 +1,5 @@
+package FanApp;
+
 /*
     Bennet, T. (2021). CIS 505 Intermediate Java Programming. Bellevue University
 */
--- Module_1/FanApp/TestFanApp.java
+++ Module_1/FanApp/TestFanApp.java
@@ -1,4 +1,4 @@
-package Module_1.FanApp;
+package FanApp;
 
 /*
     Bennet, T. (2021). CIS 505 Intermediate Java Programming. Bellevue University
