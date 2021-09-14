c001aba9b801b6a9cd0e16dada6a65d7fa9b7e03
Tyler B <tylerwbennet@gmail.com>
2021-09-13 23:37:11 -0500

    Adding boilerplate classes for Assignment 3.1

---
+++ Module_3/CustomerAccountApp/CustomerDB.java
@@ -0,0 +1,9 @@
+/*
+    Bennet, T. (2021). CIS 505 Intermediate Java Programming. Bellevue University
+*/
+
+package Module_3;
+
+public class CustomerDB {
+    
+}
---
+++ Module_3/CustomerAccountApp/TestCustomerAccountApp.java
@@ -0,0 +1,12 @@
+/*
+    Bennet, T. (2021). CIS 505 Intermediate Java Programming. Bellevue University
+*/
+
+package Module_3.CustomerAccountApp;
+
+public class TestCustomerAccountApp {
+ 
+    public static void main(String[] args) {
+
+    }
+}
